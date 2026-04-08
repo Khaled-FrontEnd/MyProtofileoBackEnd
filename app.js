1563a04f4b5bae041ce32ca8024ddad2c385aedd
Khaled-FrontEnd <k8320085@gmail.com>
2026-04-08 23:46:54 +0200

    cors intgeration 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let mongoose = require("mongoose");
 require("dotenv").config({ debug: false, silent: true });
+const cors = require('cors');
 const router = express.Router();
 
 const app = express();
