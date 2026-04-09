a0bf172eb578c9669883981bbb835c885a3ce1b7
Khaled-FrontEnd <k8320085@gmail.com>
2026-04-09 20:41:45 +0200

    arr tools

--- models/projects.js
+++ models/projects.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const projectsModel = new Schema({
-  img: { type: String, required: true},
-  name: { type: String, required: true},
-  description: { type: String, required: true},
-  demoLink: { type: String, required: true},
-  githubLink: { type: String, required: false},
-  tools: { type: String, required: false},
+  img: { type: String, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  demoLink: { type: String, required: true },
+  githubLink: { type: String, required: false },
+  tools: { type: [String], required: false, default: [] },
 });
 
 const projects = mongoose.model("projects", projectsModel);
